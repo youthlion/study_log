d937cc5b774a5abc5935083a17897a4859bf9d92
Lion Youth <2877402302@qq.com>
2022-02-19 16:25:03 +0800

    Update python_file.py

--- python_file.py
+++ python_file.py
@@ -1,12 +1,14 @@
 import os
 from pathlib import Path
 
-root_name=Path("./data/input")
 
 #列举目录里的文件名,root_path为Path类
+root_name=Path("./data/input")
 def listpath(root_path):
     names=os.listdir(root_path)
     file_names=list()
     for name in names:
         file_names.append(Path.joinpath(root_path,name))
     return file_names
+file_names=listpath(root_name)
+
